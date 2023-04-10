5fbf044d4c186eedf43b3ff3b66e66cb99589009
seiry <4397354+seiry@users.noreply.github.com>
2023-04-10 23:15:54 +0800

    update socat

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ done
 warp-cli --accept-tos set-mode proxy
 warp-cli --accept-tos set-proxy-port 40001
 warp-cli --accept-tos connect
-socat TCP-IP:localhost:40001:fork,ipv6=on TCP-IP:0.0.0.0:40000    # socat is used to redirect traffic from 40000 to 40001
+socat TCP-LISTEN:40000,fork TCP:localhost:40001  # socat is used to redirect traffic from 40000 to 40001
 ) &
 
 exec warp-svc
