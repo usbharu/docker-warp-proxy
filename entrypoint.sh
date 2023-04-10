dac42498edfbc6cc949e361193ffcdffab7be321
seiry <4397354+seiry@users.noreply.github.com>
2023-04-10 23:13:09 +0800

    add socat

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ while ! warp-cli --accept-tos register; do
 	>&2 echo "Awaiting warp-svc become online..."
 done
 warp-cli --accept-tos set-mode proxy
-warp-cli --accept-tos set-proxy-port 40000
+warp-cli --accept-tos set-proxy-port 40001
 warp-cli --accept-tos connect
+socat TCP-IP:localhost:40001:fork,ipv6=on TCP-IP:0.0.0.0:40000    # socat is used to redirect traffic from 40000 to 40001
 ) &
 
 exec warp-svc
